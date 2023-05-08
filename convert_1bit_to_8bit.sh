595cc5d537b885e9d360ca46bd061fcbf6dabd18
sattyorin <sattyorin.sattyon@gmail.com>
2023-05-08 23:50:14 +0900

    added convert_1bit_to_8bit

---
+++ convert_1bit_to_8bit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+input_dir="images"
+output_dir="converted_images"
+
+for file in "$input_dir"/*.png; do
+  output_file="$output_dir/$(basename "$file")"
+  
+  convert "$file" -depth 8 -alpha remove -alpha off "$output_file"
+  echo "Converted $file to $output_file"
+done
