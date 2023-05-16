9bbe5473f3395a3048da722bbe3446679bf5b39a
sattyorin <sattyorin.sattyon@gmail.com>
2023-05-16 21:24:35 +0900

    added ArgumentParser

--- scrape_keywords.py
+++ scrape_keywords.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import time
@@ -6,39 +7,57 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-INPUT_FILE_PATH = "icons.csv"
-OUTPUT_DIR_PATH = "keywords.csv"
-DATA_DIRECTORY = "data"
-
-PREFIX = "https://......"
-SUFFIX = ".png"
-
 SLEEP_TIME = 1
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/Chrome/84.0.4147.135 Safari/537.36"  # NOQA
 
-icons_df = pd.read_csv(os.path.join(DATA_DIRECTORY, INPUT_FILE_PATH))
+ICON_FILE_PATH = "data/icons.csv"
+OUTPUT_CSV_FILE_PATH = "data/keywords.csv"
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--prefix",
+    type=str,
+    required=True,
+    help="prefix for the image link",
+)
+parser.add_argument(
+    "--suffix",
+    type=str,
+    default=".png",
+    help="suffix for the image link",
+)
+args = parser.parse_args()
+
+# create file path
+execution_dir_path = os.path.dirname(os.path.abspath(__file__))
+output_csv_file_path = os.path.join(execution_dir_path, OUTPUT_CSV_FILE_PATH)
+
+# read icons.csv
+icons_df = pd.read_csv(os.path.join(execution_dir_path, ICON_FILE_PATH))
+
 headers = {"User-Agent": USER_AGENT}
 indexed_keywords = []
 
 for url in icons_df["url"]:
+    print(f"scraping {url}")
     time.sleep(SLEEP_TIME)
     response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         print("Error:", response.status_code)
-        exit()
+        break
 
     soup = BeautifulSoup(response.text, "html.parser")
 
     script_tag = soup.find("script", {"type": "application/ld+json"})
     data = json.loads(script_tag.string)
 
-    index = data["contentUrl"][len(PREFIX) : -len(SUFFIX)]
+    index = data["contentUrl"][len(args.prefix) : -len(args.suffix)]
     indexed_keywords.append((index, data["keywords"]))
 
-    keywords_df = pd.DataFrame.from_dict(
-        dict((index, d) for index, d in indexed_keywords), orient="index"
-    )
-
+# create DataFrame and save csv
+keywords_df = pd.DataFrame.from_dict(
+    dict((index, d) for index, d in indexed_keywords), orient="index"
+)
 keywords_df.columns = [f"keywords{column}" for column in keywords_df.columns]
-keywords_df.to_csv(os.path.join(DATA_DIRECTORY, OUTPUT_DIR_PATH))
+keywords_df.to_csv(output_csv_file_path)
--- scrape_links.py
+++ scrape_links.py
@@ -6,10 +6,14 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
 
-SAVE_CSV_FILE_PATH = "data/icons.csv"
-COLLECTION_ICON_FILE_PATH = "data/collection_icon.csv"
-SLEEP_TIME = 3
+OUTPUT_CSV_FILE_PATH = "data/icons.csv"
+COLLECTION_ICON_FILE_PATH = "data/collection_icons.csv"
+SLEEP_TIME = 1
+TIME_OUT = 10
+XPATH = "//button[contains(text(), 'Load Next 200 Icons')]"
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -22,10 +26,7 @@ args = parser.parse_args()
 
 # create file path
 execution_dir_path = os.path.dirname(os.path.abspath(__file__))
-csv_file_path = os.path.join(execution_dir_path, SAVE_CSV_FILE_PATH)
-save_csv_dir_path = os.path.join(
-    execution_dir_path, os.path.dirname(SAVE_CSV_FILE_PATH)
-)
+output_csv_file_path = os.path.join(execution_dir_path, OUTPUT_CSV_FILE_PATH)
 
 # read collection_icon.csv
 collection_icon_df = pd.read_csv(
@@ -39,17 +40,34 @@ options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(options=options)
 
 icon_links = []
-for url in collection_icon_df["url"]:
+for _, row in collection_icon_df.iterrows():
+    url = row["url"]
     driver.get(url)
-    time.sleep(SLEEP_TIME)
-    print("sleep")
+    for num_page in range(row["num_pages"]):
+        print(f"access page {num_page + 1} of {url}")
 
+        if num_page >= row["num_pages"]:
+            break
+
+        wait = WebDriverWait(driver, 10)
+        button = wait.until(
+            expected_conditions.element_to_be_clickable((By.XPATH, XPATH))
+        )
+
+        driver.execute_script("arguments[0].click();", button)
+        driver.implicitly_wait(TIME_OUT)
+
+        time.sleep(SLEEP_TIME)
+
+    print("parsing html")
     for link in driver.find_elements(By.TAG_NAME, "a"):
         href = link.get_attribute("href")
         if href and href.startswith(args.url):
             icon_links.append(href)
 
+# set DataFrame
+print("exporting DataFrame to csv")
 icons_df = pd.DataFrame(icon_links, columns=["url"])
-icons_df.to_csv(csv_file_path, index=False)
+icons_df.to_csv(output_csv_file_path, index=False)
 
 driver.quit()
